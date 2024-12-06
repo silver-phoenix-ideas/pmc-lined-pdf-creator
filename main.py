8cc76c9dd7e4963db0e4ba60ad99aad4627ad084
Silver Phoenix Ideas <silver.phoenix.ideas@gmail.com>
2024-12-06 00:48:25 -0500

    Calculate element sizes dynamically instead of hard-coding them.

--- main.py
+++ main.py
@@ -4,8 +4,22 @@ from fpdf import FPDF
 import modules.color_palette as rgb
 import modules.pdf_helper as pdf_helper
 
+# Sizes
+base = 10
+step = int(base * 1)
+header_font = int(base * 2.5)
+header_height = int(header_font / 2.5)
+footer_font = int(base * 1)
+footer_height = int(footer_font / 2)
+
 # Document
 paper = pdf_helper.get_paper("A4")
+paper["margin_bottom"] = 10
+
+area = pdf_helper.calculate_area(paper)
+area["top"] += header_height
+area["bottom"] -= footer_height
+
 pdf = FPDF("portrait", "mm", paper["format"])
 pdf.set_auto_page_break(False, 0)
 pdf.set_text_color(*rgb.grey)
@@ -18,21 +32,19 @@ for i, row in data.iterrows():
         pdf.add_page()
 
         # Header
-        txt_header = row["Topic"] if not j else ""
-        pdf.set_font("Times", "B", 24)
-        pdf.cell(0, 12, txt_header, 0, 1, "L")
+        header_txt = row["Topic"] if not j else ""
+        pdf.set_font("Times", "B", header_font)
+        pdf.cell(0, header_height, header_txt, 0, 0, "L")
 
-        draw_color = rgb.grey if not j else rgb.white
+        draw_color = rgb.dark_grey if not j else rgb.white
         pdf.set_draw_color(*draw_color)
-        pdf.line(10, 21, 200, 21)
-
-        # Blank Space
-        pdf.ln(265)
+        pdf.line(area["left"], area["top"], area["right"], area["top"])
 
         # Footer
-        txt_footer = row["Topic"] + " (cont.)" if j else row["Topic"]
-        pdf.set_font("Times", "I", 8)
-        pdf.cell(0, 10, txt_footer, 0, 1, "R")
+        pdf.ln(area["bottom"] - area["top"] + header_height)
+        footer_txt = row["Topic"] + " (cont.)" if j else row["Topic"]
+        pdf.set_font("Times", "I", footer_font)
+        pdf.cell(0, footer_height, footer_txt, 0, 0, "R")
 
 # Output File
 pdf.output("files/python-study-notebook.pdf")
